66be5f3d0310a7f14c2a01839d829550e49df7ff
Justin Hair <73206799+jinst255@users.noreply.github.com>
2026-04-23 19:19:47 -0600

    Finished Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,31 @@
+/* 
+Author: Justin Hair
+
+Sources:
+Starter code from template on main github repo
+
+I learned the most from the video provided at: 
+https://byui-cse.github.io/cse210-course-2023/unit01/csharp-1.html
+As well as by asking GPT-4.1 general questions about C#
+
+I also messed around with Console.Beep() but found it didnt work on linux. 
+*/
+
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Clear();
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        
+        Console.Write($"\nYour name is ");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{last_name}, {first_name} {last_name}."); 
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,19 @@
 using System;
 
+//Hey this is a comment
+/* This
+is a multi line comment
+*/
+
 class Program
 {
     static void Main(string[] args)
     {
+        int x = 5;
+        Console.WriteLine(x); 
+        Console.Write("Enter the data: "); 
+        string text = Console.ReadLine();
         Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine(text);
     }
-}
+} 
