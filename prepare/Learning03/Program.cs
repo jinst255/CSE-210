66bd8b88e5743d2e3b416aa176392e20c2082f2f
Justin Hair <73206799+jinst255@users.noreply.github.com>
2026-05-19 22:06:57 -0600

    Completed Prep 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        /*
+        Fraction f = new Fraction(5);
+        // f.SetTop(1);
+        // f.SetBottom(2);
+        Console.WriteLine($"The fraction is: {f.GetFractionString()}");
+        Console.WriteLine($"The decimal value is: {f.GetDecimalValue()}");
+        */
+
+        // initiate the fraction and random number generator
+        Fraction myFraction = new Fraction();
+        Random random = new Random();
+
+        for (int i = 0; i < 25; i++)
+        {
+            // Get 2 random ints
+            int randTop = random.Next(1, 11);
+            int randBottom = random.Next(1, 11);
+
+            // Set the fraction values
+            myFraction.SetTop(randTop);
+            myFraction.SetBottom(randBottom);
+
+            // Get the string and double version of our fraction and print it out
+            string fracString = myFraction.GetFractionString();
+            double decimalValue = myFraction.GetDecimalValue();
+            Console.WriteLine($"Fraction 1: string: {fracString} Number: {decimalValue}");
+        }
+
     }
 }
+
+
+
+
