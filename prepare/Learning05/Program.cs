2085650dcab210fd5eee8cce1de5f86667196eb6
Justin Hair <73206799+jinst255@users.noreply.github.com>
2026-06-15 20:22:40 -0600

    Completed Learning 04

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine("Hello Learning05 World!\n"); 
+
+        /* 
+        // This doesnt scale well
+        Square firstShape = new Square("Blue", 3);
+        Rectangle secondShape = new Rectangle("Red", 4, 5);
+        Circle thirdShape = new Circle("Green", 2);
+
+        List <Shape> shapes = new List<Shape>();
+        shapes.Add(firstShape);
+        shapes.Add(secondShape);
+        shapes.Add(thirdShape);
+        */
+
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Yellow", 7),
+            new Square("Blue", 2),
+            new Square("Pink", 5),
+            new Rectangle("Red", 4, 7),
+            new Rectangle("Red", 4, 5),
+            new Circle("Green", 9),
+            new Circle("Blue", 2),
+            new Circle("Red", 3)
+        };
+
+
+        string color;
+        double area;
+
+        foreach (Shape shape in shapes)
+        {
+            color = shape.GetColor();
+            area = shape.GetArea();
+            Console.WriteLine($"The area of the {color} shape is: {area}");
+        }
     }
 }
